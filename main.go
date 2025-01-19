fcb3914162f80c6cc286b994b7d42a2421f701b1
Lukecell <alex@thesalleys.com>
2025-01-18 23:17:47 -0800

    Improved error handling and architecture. Just need to add uploading functionality and this project will probably be considered complete

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"log"
 	"net"
 	"fmt"
-	"encoding/binary"
-	"unsafe"
 	"strconv"
 	"bufio"
 	"os"
@@ -13,26 +11,15 @@ import (
 	"bytes"
 )
 
-var networkOrder binary.ByteOrder = binary.BigEndian
-
 const NOT_LOGIN  = 530
 const SUCC_LOGIN = 230
 const SUCC_USERNAME = 331
 const SERVICE_READY = 220
 const PASSV_LNK_READY = 227
 
-func findEndian() binary.ByteOrder {
-	var i int = 0x0100
-	ptr := unsafe.Pointer(&i)
-	if 0x01 == *(*byte)(ptr) {
-	    return binary.BigEndian
-	} else if 0x00 == *(*byte)(ptr) {
-	    return binary.LittleEndian
-	} else {
-		log.Fatalf("Failed to determine local system endianness")
-	}
-
-	return networkOrder // default to make compiler happy
+type commandInfo struct {
+	commandPrefix   string
+	commandFunction func(string, net.Dialer, *net.Conn) bool
 }
 
 func ReceiveData(commandConn net.Conn, dataConn net.Conn, buff []byte) []byte {
@@ -50,7 +37,6 @@ func ReceiveData(commandConn net.Conn, dataConn net.Conn, buff []byte) []byte {
 
 			return receivedData
 		} else {
-			fmt.Println("appending...")
 			receivedData = append(receivedData, buff...)
 
 		}
@@ -60,6 +46,9 @@ func ReceiveData(commandConn net.Conn, dataConn net.Conn, buff []byte) []byte {
 }
 
 func SendFTPcontrolMessage(tcpConnection net.Conn, message string, buff []byte) (int, string) {
+	if tcpConnection == nil {
+		return NOT_LOGIN, ""
+	}
 	// FTP receives commands in ascii byte form, terminated by \r\n
 	outputMessage := make([]byte,len(message) + 2)
 	for i:=0; i<len(message); i++ {
@@ -92,12 +81,12 @@ func SendFTPcontrolMessage(tcpConnection net.Conn, message string, buff []byte) 
 /*
 * Establishes an FTP data connection using passive mode
 */
-func EstablishDataConnection(commandConnection net.Conn, dialer net.Dialer, buff[]byte) net.Conn {
+func EstablishDataConnection(controlConnection net.Conn, dialer net.Dialer, buff[]byte) (net.Conn, error){
 	var ip [6] int
-	retCode, retMsg := SendFTPcontrolMessage(commandConnection, "PASV", buff)
+	retCode, retMsg := SendFTPcontrolMessage(controlConnection, "PASV", buff)
 
 	if retCode != PASSV_LNK_READY {
-		return nil
+		return nil, fmt.Errorf("%v", retMsg)
 	}
 
 	_, err := fmt.Sscanf(retMsg, "Entering Passive Mode (%d,%d,%d,%d,%d,%d).", &ip[0], &ip[1], &ip[2], &ip[3], &ip[4], &ip[5])
@@ -108,11 +97,11 @@ func EstablishDataConnection(commandConnection net.Conn, dialer net.Dialer, buff
 	addrString := strconv.Itoa(ip[0]) + "." +strconv.Itoa(ip[1])+"." +strconv.Itoa(ip[2])+"." +strconv.Itoa(ip[3]) +":"+strconv.Itoa(ip[4]*256 + ip[5])
 	dataConn, err := dialer.Dial("tcp", addrString)
 	if err == nil {
-		return dataConn
+		return dataConn, nil
 	}
 
 	log.Fatalf("Failed to dial: %v", err)
-	return nil
+	return nil, fmt.Errorf("Failed to connect to server's data port. Hell if I know why")
 }
 
 func Login(activeConnection net.Conn, reader *bufio.Reader, buff []byte) bool {
@@ -156,11 +145,89 @@ func CleanBuffer(buff []byte) {
 	}
 }
 
-func main() {
-	var dataConn net.Conn
-	var d net.Dialer
+func ConnectFTP(strippedCommand string, dialer net.Dialer, controlConnection *net.Conn) bool {
+	var err error
+	var buff = make([]byte, 100)
+	fmt.Println(strippedCommand)
+	*controlConnection, err = dialer.Dial("tcp", strippedCommand)
+
+
+
+	if err != nil {	
+		fmt.Printf("Failed to dial: %v\n", err)
+		return false
+	}
+
+	(*controlConnection).Read(buff)
+	responseCode,err := strconv.ParseInt(string(buff)[:3], 10, 16)
+	if err != nil {
+		log.Fatalf("Failed to retrieve error code: %v", err)
+	}
+
+	if responseCode == SERVICE_READY {
+		fmt.Println("Connection established with " + strippedCommand)
+		return true
+	} else {
+		fmt.Println("Connection could not be established with " + strippedCommand)
+		controlConnection = nil
+		return false
+	}
+}
+
+//TODO: better error handling
+func List(strippedCommand string, dialer net.Dialer, controlConnection *net.Conn) bool {
 	buff := make([]byte, 1024)
-	
+	dataConn,err := EstablishDataConnection(*controlConnection, dialer, buff)
+	if  err != nil {
+		fmt.Println(err)
+		return false
+	}
+	SendFTPcontrolMessage(*controlConnection, "LIST", buff)
+	receivedData := ReceiveData(*controlConnection, dataConn, buff)
+	fmt.Println("Printing received data...")
+	fmt.Println(string(receivedData[:]))
+	return true
+}
+
+func RetrieveFile(filename string, dialer net.Dialer, controlConnection *net.Conn) bool {
+	buff := make([]byte, 1024)
+	dataConn, err := EstablishDataConnection(*controlConnection, dialer, buff)
+	if  err != nil {
+		fmt.Println(err)
+		return false
+	}
+	SendFTPcontrolMessage(*controlConnection, "RETR " + filename, buff)
+	receivedData := ReceiveData(*controlConnection, dataConn, buff)
+	receivedData = bytes.TrimRight(receivedData, "\x00")
+	err = os.WriteFile(filename, receivedData, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return true
+}
+
+func main() {
+	var validCommands = []commandInfo {
+		commandInfo {
+			commandPrefix: "open ", 
+			commandFunction: ConnectFTP,
+		},
+		commandInfo {
+			commandPrefix: "list", 
+			commandFunction: List,
+		},
+		commandInfo {
+			commandPrefix: "get ", 
+			commandFunction: RetrieveFile,
+		},
+	}
+
+
+//	var dataConn net.Conn
+	var d net.Dialer
+	var controlConn net.Conn
+	buff := make([]byte, 1024)
+/*	
 	controlConn, err := d.Dial("tcp", "10.0.0.7:21")
 	if err != nil {
 		log.Fatalf("Failed to dial: %v", err)
@@ -168,7 +235,6 @@ func main() {
 	defer controlConn.Close()
 
 	if err != nil {
-		log.Fatalf("Failed to dial: %v", err)
 	}
 
 	for true{
@@ -178,15 +244,42 @@ func main() {
 			break
 		}
 	}
+	*/
 
+//	loggedIn := false
 	reader := bufio.NewReader(os.Stdin)
 	for true {
 		CleanBuffer(buff)
+//		commandExecuted := false
 		fmt.Print("Enter command: ")
 		userInput := ReadUserInput(reader)
+
+		if userInput=="quit" || userInput == "q" {
+			fmt.Println("quitting")
+			if controlConn != nil {
+				controlConn.Close()
+			}
+
+			return
+		}
+
+		for _,command := range validCommands {
+			if strings.HasPrefix(userInput, command.commandPrefix){
+				strippedCommand := strings.TrimPrefix(userInput, command.commandPrefix)
+				command.commandFunction(strippedCommand, d, &controlConn)
+//				commandExecuted = false
+				break
+			}
+		}
+
+		if controlConn == nil {
+			fmt.Println("No current connection. Connect to host")
+		}
+
+
 		if strings.ToLower(userInput) == "login" {
 			Login(controlConn, reader, buff)
-		} else if strings.ToLower(userInput) == "ls" {
+		} /*else if strings.ToLower(userInput) == "ls" {
 			dataConn = EstablishDataConnection(controlConn, d, buff)
 			SendFTPcontrolMessage(controlConn, "list", buff)
 			receivedData := ReceiveData(controlConn, dataConn, buff)
@@ -215,10 +308,7 @@ func main() {
 			controlConn.Read(buff)
 		} else {
 			fmt.Println(SendFTPcontrolMessage(controlConn, userInput, buff))
-		}
+		}*/
 	}
 
-	fmt.Println(SendFTPcontrolMessage(controlConn, "PASS bftp", buff))
-
-	fmt.Println(SendFTPcontrolMessage(controlConn, "QUIT", buff))
 }
